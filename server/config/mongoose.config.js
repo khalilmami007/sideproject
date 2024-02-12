76ee64300ce7c5921be05f87eed8eacb755ab8e5
khalilmami007 <136359722+khalilmami007@users.noreply.github.com>
2024-02-12 23:23:42 +0100

    .

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,9 +1,12 @@
-const mongoose=require ('mongoose');
+const mongoose = require("mongoose");
 
-const dbName=process.env.DB
+const dbName = process.env.DB;
 
-mongoose.connect(`mongodb://127.0.0.1:27015/${dbName}`)
-
-.then(()=> {console.log(`Established a connection to Database ${dbName}`)})
-
-.catch(err  => console.log("something went wrong ",err))
+mongoose
+  .connect(`mongodb://127.0.0.1:27017/${dbName}`)
+  .then(() => {
+    console.log(`Established a connection to the database ${dbName}`);
+  })
+  .catch((err) =>
+    console.log("Something Went wrong when connecting to the DB ", err)
+  );
--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
 const express=require('express')
+const cors=require('cors') 
+
+const app =express()
+
+app.use(express.json(),express.urlencoded({extended:true}),cors());
+
+require("dotenv").config()
+require('./config/mongoose.config')
+
+const port=process.env.PORT 
+
+app.listen(port,()=>{
+    console.log(`server is running on Port ${port} `)
+
+})
